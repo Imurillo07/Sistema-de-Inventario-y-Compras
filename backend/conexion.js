b68e33e882e236856b77005af2be7fecf5823ed7
Isaac Murillo <isaacmurillo07@gmail.com>
2025-08-03 19:55:11 -0600

    trabajamos en el index y creamos el file conexions js para connectar la base se datos pero aun falta indformacion en este

---
+++ backend/conexion.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql')
+
+//configuracion para la conexion a la base de datos 
+const dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    databas: 'db_inv_ti'
+}
+
+const db = mysql.createConnection(dbConfig)
+
+//connectar a la base de datos
+db.connect((err) => {
+    if(err) {
+        console.error('Error  al connectar a la base de datos: ', err)
+    }
+
+    console.log('Conectado a la base de datos MySQL')
+})
+
+module.exports = db
---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const usuariosRoutes = require('./usuariosRoutes')
+const areasRoutes = require('./areasRoutes')
+const equiposRoutes = require('./equiposRoutes')
+const productosRoutes = require('./productosRoutes')
+const ventasRoutes = require('./ventasRoutes')
+const e = require('cors')
+
+//crear instancias de express
+const app = express()
+
+//permitir solicutudes de otros dominios
+app.use,(cors())
+
+//middleware para analizar json
+app.use(bodyParser.json())
+
+//importamos el uso de las rutas
+app.use('/', usuariosRoutes)
+app.use('/', areasRoutes)
+app.use('/', equiposRoutes)
+app.use('/', productosRoutes)
+app.use('/', ventasRoutes)
+
+//iniciar el servidor
+const port = 3000
+app.listen(port, () => {
+    console.log(`Servidor ejecutandose en http://localhost:${port}`)
+})
