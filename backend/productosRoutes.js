94df8e97cd300e578b6844b7033af50c4d9ba945
Isaac Murillo <isaacmurillo07@gmail.com>
2025-08-07 22:06:23 -0600

    anadimos file productosRoutes en el cual estaran la rutas de los productos, se empezaron las rutas pero todavia hay unas cuantas pendientes

---
+++ backend/productosRoutes.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const router = express.Router()
+const db = require('./conexion')
+
+//ruta para obtener los producto de la tabla productos
+router.get('/productos', (req, res) =>{
+
+    const query = 'SELECT * FROM productos'
+
+    db.query(query, (err, results) => {
+        if (err) {
+            return res.status(500).send('Error en la consulta')
+        }
+
+        res.json(results)
+    })
+})
+
+//ruta para obtener el producto usando el codigo mismo
+router.get('/producto', (req, res) => {
+    const { codigo } = req.query
+
+    const query = 'SELECT codigo, nom_producto, pre_publico FROM productos WHERE codigo = ?'
+    db.query(query, [codigo], (err, results) =>{
+        if (err) {
+            return res.status(500).send('Error al obtener el producto')
+        }
+
+        res.json(result)
+    })
+})
+
+//ruta para agregar un nuevo producto
+router.post('/productos', (req, res) => {
+    const { codigo, nom_producto, desc_producto, pre_publico, pre_proveedor, existencias } = req.body
+
+    if(!codigo || !nom_producto || !desc_producto || !pre_publico || !pre_proveedor || !existencias) {
+        return res.status(400).send('Todos los campos son obligatorios')    
+    }
+
+    const query = `INSERT INTO productos(codigo, nom_producto, desc_producto, pre_publico, pre_proveedor, existencias)
+    VALUES(?,?,?,?,?,?)`
+
+    db.query(query, [codigo, nom_producto, desc_producto, pre_publico, pre_proveedor, existencias], (err, result) =>{
+        if(err) {
+            console.error('Error al agregar el producto: ', err)
+            return res.status(500).send('Error al agregar el producto')
+        }
+
+        res.sendStatus(201).send({
+            codigo, nom_producto, desc_producto, pre_publico, pre_proveedor, existencias
+        })
+    }) 
+})
+
+//ruta para editar in producto 
+router.put('/productos/:codigo', (req, res) =>{
+    const {codigo } = req.params
+    const { nom_producto, desc_producto, pre_publico, pre_proveedor, existencias } = req.body
+
+    const query = 'UDPATE productos SET nom_producto=?, desc_producto=?, pre_publico=?, pre_proveedor=?, existencias=? WHERE codigo=?'
+    
+})
